f9d25b35a81071a30c403a6bb52a1e6c6a336ea4
chigov2 <chigov@gmail.com>
2021-11-10 15:58:26 +0200

    Initial commit

---
+++ app/src/main/java/techmarket/uno/javasecond/MainActivity.java
@@ -0,0 +1,14 @@
+package techmarket.uno.javasecond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
