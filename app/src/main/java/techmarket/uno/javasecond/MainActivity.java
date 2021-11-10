254b6afaf00fa1ebc35c77af8dd2c698954040ef
chigov2 <chigov@gmail.com>
2021-11-10 16:59:38 +0200

    2

--- app/src/main/java/techmarket/uno/javasecond/MainActivity.java
+++ app/src/main/java/techmarket/uno/javasecond/MainActivity.java
@@ -3,12 +3,34 @@ package techmarket.uno.javasecond;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinnerColors;
+    private TextView tvDescriptionTemperament;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //находим элементы из макета
+        spinnerColors = findViewById(R.id.spinnerColors);
+        tvDescriptionTemperament = findViewById(R.id.tvDescriptionTemperament);
+    }
+
+    public void showDescription(View view) {
+        int position = spinnerColors.getSelectedItemPosition();
+        String description = getDescriptionByPosition(position);
+        tvDescriptionTemperament.setText(description);
+    }
+    //новый метод - возвращает описание характера -возвращает строку,
+    // а в качастве параметра получаем номер позиции єлемента
+    private String getDescriptionByPosition(int position){
+        //создаем массив строк с описанием характера
+        String[] descriptions = getResources().getStringArray(R.array.temperament_description);
+        return descriptions[position];
     }
 }
